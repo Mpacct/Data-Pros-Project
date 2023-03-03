24ae7a964f4724424a46945cc3dd86bc3296c71b
unknown <marcuspacc@gmail.com>
2023-03-02 21:05:53 -0700

    updated route and js files

--- public/js/login.js
+++ public/js/login.js
@@ -10,11 +10,11 @@ const loginHandler = async (event) => {
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password}),
-            headers: {'Content-Type' : 'applicaiton/json'},
+            headers: {'Content-Type' : 'application/json'},
         });
 
         if (response.ok) 
-        {
+        {        
             document.location.replace('/');
         }
         else 
